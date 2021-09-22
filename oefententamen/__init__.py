d2438806f3794ccc8310fea7ace9f295d851cec1
jelleas <jelle.van.assema@gmail.com>
2021-09-22 17:10:40 +0200

    trein example checks

--- oefententamen/__init__.py
+++ oefententamen/__init__.py
@@ -1,9 +1,17 @@
 import check50
+import check50.c
 
 @check50.check()
 def trein():
     """trein.c is correct"""
     check50.exists("trein.c")
+    check50.c.compile("trein.c")
+
+    # check example 1
+    check50.run("./trein").stdin("100").stdin("50").stdout("11,75").stdout("59")
+
+    # check example 2
+    check50.run("./trein").stdin("20").stdin("75").stdout("5.63").stdout("141")
 
 @check50.check()
 def babysitten():
