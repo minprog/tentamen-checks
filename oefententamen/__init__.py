5eb52dcc9a4f3b6b9e50eb60412a0559ea7bdc28
jelleas <jelle.van.assema@gmail.com>
2021-09-22 17:03:59 +0200

    init

---
+++ oefententamen/__init__.py
@@ -0,0 +1,26 @@
+import check50
+
+@check50.check()
+def trein():
+    """trein.c is correct"""
+    check50.exists("trein.c")
+
+@check50.check()
+def babysitten():
+    """babysitten.c is correct"""
+    check50.exists("babysitten.c")
+
+@check50.check()
+def tram():
+    """tram.c is correct"""
+    check50.exists("tram.c")
+
+@check50.check()
+def driehoek():
+    """driehoek.c is correct"""
+    check50.exists("driehoek.c")
+
+@check50.check()
+def temperaturen():
+    """temperaturen.c is correct"""
+    check50.exists("temperaturen.c")
