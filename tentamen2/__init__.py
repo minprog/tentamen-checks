d6b63dd683d60ef012b6e66744e4453656e484fe
jelleas <jelle.van.assema@gmail.com>
2021-10-22 12:11:11 +0000

    regen checks

--- tentamen2/__init__.py
+++ tentamen2/__init__.py
@@ -7,11 +7,10 @@ import sys
 @check50.check()
 def rechthoeken():
     """rechthoeken is correct"""
-    NAME = "rechthoeken"
-    COMMAND = make_runnable(NAME)
+    command = make_runnable("rechthoeken")
 
     # check example 1
-    (check50.run(COMMAND)
+    (check50.run(command)
         .stdin("45")
         .stdin("33")
         .stdin("22")
@@ -20,7 +19,7 @@ def rechthoeken():
         .stdout("1221(?!\d)", str_output="1221"))
 
     # check example 2
-    (check50.run(COMMAND)
+    (check50.run(command)
         .stdin("1")
         .stdin("43")
         .stdin("39")
@@ -29,7 +28,7 @@ def rechthoeken():
         .stdout("1252(?!\d)", str_output="1252"))
 
     # check example 3
-    (check50.run(COMMAND)
+    (check50.run(command)
         .stdin("1")
         .stdin("43")
         .stdin("39")
@@ -38,7 +37,7 @@ def rechthoeken():
         .stdout("43(?!\d)", str_output="43"))
 
     # check example 4
-    (check50.run(COMMAND)
+    (check50.run(command)
         .stdin("1")
         .stdin("43")
         .stdin("39")
@@ -47,7 +46,7 @@ def rechthoeken():
         .stdout("is geen geldige keuze"))
 
     # check example 5
-    (check50.run(COMMAND)
+    (check50.run(command)
         .stdin("1")
         .stdin("43")
         .stdin("39")
@@ -59,28 +58,60 @@ def rechthoeken():
 @check50.check()
 def hoofdletters():
     """hoofdletters is correct"""
-    NAME = "hoofdletters"
-    COMMAND = make_runnable(NAME)
+    command = make_runnable("hoofdletters")
 
     # check example 1
-    (check50.run(COMMAND)
+    (check50.run(command)
         .stdin("Er zijn geen goede schrijvers.")
         .stdout("1 woord met een hoofdletter"))
 
     # check example 2
-    (check50.run(COMMAND)
+    (check50.run(command)
         .stdin("Obi-Wan Kenobi nam zijn taak vrij serieus")
         .stdout("2 woorden met een hoofdletter"))
 
     # check example 3
-    (check50.run(COMMAND)
+    (check50.run(command)
         .stdin("Het leven op zondag begon zonder Onrust.")
         .stdout("2 woorden met een hoofdletter"))
 
     # check example 4
-    (check50.run(COMMAND)
+    (check50.run(command)
         .stdin("ergens heeft het ook wel wat")
         .stdout("0 woorden met een hoofdletter"))
+
+
+@check50.check()
+def regen():
+    """regen is correct"""
+    command = make_runnable("regen")
+
+    # check example 1
+    (check50.run(command)
+        .stdin("12")
+        .stdin("12")
+        .stdin("999")
+        .stdout("Gemiddeld 12 millimeter"))
+
+    # check example 2
+    (check50.run(command)
+        .stdin("12")
+        .stdin("6")
+        .stdin("3")
+        .stdin("999")
+        .stdout("Gemiddeld 7 millimeter"))
+
+    # check example 3
+    (check50.run(command)
+        .stdin("999")
+        .stdout("Dat kan niet"))
+
+    # check example 4
+    (check50.run(command)
+        .stdin("12")
+        .stdin("11")
+        .stdin("999")
+        .stdout("Gemiddeld 11 millimeter"))
 
 
 def make_runnable(name):
