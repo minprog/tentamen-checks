116bb4488ffe76e32018567a579cac2e1827fbae
jelleas <jelle.van.assema@gmail.com>
2021-10-22 11:21:13 +0000

    py and c accepted, hoofdletters check

--- tentamen2/__init__.py
+++ tentamen2/__init__.py
@@ -1,41 +1,44 @@
 import check50
 import check50.c
+import os
+import sys
+
 
 @check50.check()
 def rechthoeken():
-    """rechthoeken.c is correct"""
-    check50.exists("rechthoeken.c")
-    check50.c.compile("rechthoeken.c", "-lcs50")
+    """rechthoeken is correct"""
+    NAME = "rechthoeken"
+    COMMAND = make_runnable(NAME)
 
     # check example 1
-    (check50.run("./rechthoeken")
+    (check50.run(COMMAND)
         .stdin("45")
         .stdin("33")
         .stdin("22")
         .stdin("12")
         .stdin("V")
-        .stdout("1221(?!\d)", str_output="12.21"))
+        .stdout("1221(?!\d)", str_output="1221"))
 
     # check example 2
-    (check50.run("./rechthoeken")
+    (check50.run(COMMAND)
         .stdin("1")
         .stdin("43")
         .stdin("39")
         .stdin("31")
         .stdin("S")
-        .stdout("1252(?!\d)", str_output="12.52"))
+        .stdout("1252(?!\d)", str_output="1252"))
 
     # check example 3
-    (check50.run("./rechthoeken")
+    (check50.run(COMMAND)
         .stdin("1")
         .stdin("43")
         .stdin("39")
         .stdin("31")
         .stdin("1")
-        .stdout("43(?!\d)", str_output="0.43"))
+        .stdout("43(?!\d)", str_output="43"))
 
     # check example 4
-    (check50.run("./rechthoeken")
+    (check50.run(COMMAND)
         .stdin("1")
         .stdin("43")
         .stdin("39")
@@ -44,10 +47,48 @@ def rechthoeken():
         .stdout("is geen geldige keuze"))
 
     # check example 5
-    (check50.run("./rechthoeken")
+    (check50.run(COMMAND)
         .stdin("1")
         .stdin("43")
         .stdin("39")
         .stdin("31")
         .stdin("0")
         .stdout("is geen geldige keuze"))
+
+
+@check50.check()
+def hoofdletters():
+    """hoofdletters is correct"""
+    NAME = "hoofdletters"
+    COMMAND = make_runnable(NAME)
+
+    # check example 1
+    (check50.run(COMMAND)
+        .stdin("Er zijn geen goede schrijvers.")
+        .stdout("1 woord met een hoofdletter"))
+
+    # check example 2
+    (check50.run(COMMAND)
+        .stdin("Obi-Wan Kenobi nam zijn taak vrij serieus")
+        .stdout("2 woorden met een hoofdletter"))
+
+    # check example 3
+    (check50.run(COMMAND)
+        .stdin("Het leven op zondag begon zonder Onrust.")
+        .stdout("2 woorden met een hoofdletter"))
+
+    # check example 4
+    (check50.run(COMMAND)
+        .stdin("ergens heeft het ook wel wat")
+        .stdout("0 woorden met een hoofdletter"))
+
+
+def make_runnable(name):
+    if os.path.exists(f"{name}.c"):
+        check50.c.compile(f"{name}.c", "-lcs50")
+        return f"./{name}"
+
+    if os.path.exists(f"{name}.py"):
+        return f"{sys.executable} {name}.py"
+
+    raise check50.Failure(f"{name} is niet aanwezig")
