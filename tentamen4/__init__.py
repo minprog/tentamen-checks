6460b898fb572a45c6e8b8450ccac7c6cf4a4b3d
jelleas <jelle.van.assema@gmail.com>
2021-10-24 13:48:13 +0000

    elektrisch rijden checks

--- tentamen4/__init__.py
+++ tentamen4/__init__.py
@@ -2,7 +2,7 @@ import check50
 import check50.c
 import os
 import sys
-
+import re
 
 @check50.check()
 def reistijd():
@@ -24,6 +24,59 @@ def reistijd():
         .stdin("F")
         .stdin("3")
         .stdout("reis kost je ongeveer 73 minuten", regex=False))
+
+
+@check50.check()
+def elektrisch_rijden():
+    """elektrisch rijden is correct"""
+    command = make_runnable("elektrisch")
+
+    def to_regex(answer):
+        # escape all |
+        answer_regex = [re.sub("\|", "\|", l) for l in answer]
+
+        # escape all .
+        answer_regex = [re.sub("\.", "\.", l) for l in answer_regex]
+
+        # replace all spaces with "[ ]*"
+        return [re.sub("[ ]+", "[ ]*", l) for l in answer_regex]
+
+    # check example 1
+    check = check50.run(command).stdin("1.95").stdin("0.3")
+    answer = [
+        "| KM    | Elektrisch | Benzine  |\n",
+        "| 100   |  6.00      |  7.80    |\n",
+        "| 200   | 12.00      | 15.60    |\n",
+        "| 300   | 18.00      | 23.40    |\n",
+        "| 400   | 24.00      | 31.20    |\n",
+        "| 500   | 30.00      | 39.00    |\n",
+        "| 600   | 36.00      | 46.80    |\n",
+        "| 700   | 42.00      | 54.60    |\n",
+        "| 800   | 48.00      | 62.40    |\n",
+        "| 900   | 54.00      | 70.20    |"
+    ]
+
+    for line, line_regex in zip(answer, to_regex(answer)):
+        check.stdout(line_regex, str_output=line)
+
+    # check example 2
+    check = check50.run(command).stdin("2.1").stdin("0.33")
+    answer = [
+        "| KM    | Elektrisch | Benzine  |\n",
+        "| 100   |  6.60      |  8.40    |\n",
+        "| 200   | 13.20      | 16.80    |\n",
+        "| 300   | 19.80      | 25.20    |\n",
+        "| 400   | 26.40      | 33.60    |\n",
+        "| 500   | 33.00      | 42.00    |\n",
+        "| 600   | 39.60      | 50.40    |\n",
+        "| 700   | 46.20      | 58.80    |\n",
+        "| 800   | 52.80      | 67.20    |\n",
+        "| 900   | 59.40      | 75.60    |"
+    ]
+
+    for line, line_regex in zip(answer, to_regex(answer)):
+        check.stdout(line_regex, str_output=line)
+
 
 
 def make_runnable(name):
