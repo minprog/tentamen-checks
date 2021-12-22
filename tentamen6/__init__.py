6c675dbdf879cca8bff0ef184f5e1d885d64d317
Martijn Stegeman <martijn@stgm.nl>
2021-12-22 10:16:36 +0100

    betere check

--- tentamen6/__init__.py
+++ tentamen6/__init__.py
@@ -21,7 +21,7 @@ def dobbelsteen():
         for i in inputs:
             run = run.stdin(str(i))
         run = run.stdin("0")
-        run.stdout("Totalen:\n")
+        run.stdout("Totalen:\s*")
         run.stdout("\n".join([f"{k}: {output.get(k,0)}" for k in [1,2,3,4,5,6]]))
 
         # check length 0 input
