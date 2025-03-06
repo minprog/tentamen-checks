d07b90797dc3e3afd3fd00fd7c1f288730be5008
jelleas <jelle.van.assema@gmail.com>
2025-03-06 14:50:36 +0100

    tentamen20 checks

--- tentamen20/__init__.py
+++ tentamen20/__init__.py
@@ -6,6 +6,8 @@ helpers = check50.internal.import_file(
     "helpers",
     check50.internal.check_dir / "../helpers/helpers.py"
 )
+helpers.set_stdout_limit(10000)
+
 
 @check50.check()
 def raster():
